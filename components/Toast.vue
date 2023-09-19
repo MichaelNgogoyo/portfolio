a12a6b81c19acc4365fe26cc73f4a748f616fce0
root <root@localhost.localdomain>
2023-09-20 00:41:41 +0300

    minor changes

--- components/Toast.vue
+++ components/Toast.vue
@@ -1,4 +1,5 @@
 <template>
+
   <div :class="classes.container" class="flex py-2 px-1 mb-2 rounded relative space-x-3">
       <span v-if="theme === 'success'" class="flex shrink-0 grow-0 justify-center items-center h-5 w-5 rounded-full bg-green-200 ">
         <svg xmlns="http://www.w3.org/2000/svg" class="fill-green-700" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M470.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L192 338.7 425.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"/></svg>
